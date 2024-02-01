6eb5ead6cea898f5e15fb78ebca49fa81b59a26f
Stafword <konovala28@gmail.com>
2024-02-01 18:47:04 +0100

    Update hw_3_3.py

--- hw_3_3.py
+++ hw_3_3.py
@@ -1,6 +1,5 @@
 import re
 
-
 raw_numbers = [
     "067\\t123 4567",
     "(095) 234-5678\\n",
@@ -12,18 +11,24 @@ raw_numbers = [
     "38050-111-22-22",
     "38050 111 22 11   ",
 ]
+dirty_phones = [
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
 
 
 def normalize_phone(phone_number):
-    cleaned_number = re.sub(r"\D", "", phone_number)
+    p1 = r"[\d\+]+"
+    phone_number = "".join(re.findall(p1, phone_number))
+    if len(phone_number) == 10:
+        phone_number = "+38" + phone_number
+    elif len(phone_number) == 12:
+        phone_number = "+" + phone_number
+    return phone_number
 
-    if cleaned_number.startswith("+"):
-        if len(cleaned_number) == 12 and cleaned_number[1:4] != "38":
-            cleaned_number = "+38" + cleaned_number[1:]
-    else:
-        cleaned_number = "+38" + cleaned_number
-    return cleaned_number
 
-
-sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
-print("Нормализовані номери телефонів для SMS-розсилки:", sanitized_numbers)
+for phone in raw_numbers:
+    print(normalize_phone(phone))
