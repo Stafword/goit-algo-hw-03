93c2b5dca99aaca08d613b9ba95d43027058468e
Stafword <konovala28@gmail.com>
2024-02-02 14:26:39 +0100

    Update hw_3_2.py

--- hw_3_2.py
+++ hw_3_2.py
@@ -2,12 +2,13 @@ import random
 
 
 def get_numbers_ticket(min, max, quantity):
-    if not (1 <= min <= max <= 1000) or quantity <= 0:
+    if not (1 <= min <= max <= 1000) or not (1 <= quantity <= max - min + 1):
+        print("Ви ввели некоректні вхідні дані. Перевірьте коректність даних")
         return []
 
     numbers = random.sample(range(min, max + 1), quantity)
     return sorted(numbers)
 
 
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 14, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
